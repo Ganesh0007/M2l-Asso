1c88ac5512aebaeba209fd25a3f2bf9ae368cf95
Ganesh <radjeganesh@hotmail.fr>
2019-04-30 00:48:44 +0200

    Publication en cours + site opé

--- M2L_Asso/Controllers/HomeController.cs
+++ M2L_Asso/Controllers/HomeController.cs
@@ -3,6 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Mail;
+
+using M2L_Asso.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Threading.Tasks;
 
 namespace M2L_Asso.Controllers
 {
@@ -22,11 +28,44 @@ namespace M2L_Asso.Controllers
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Contact(Email model)
+        {
+            if (ModelState.IsValid)
+            {
+                var body = "<p>Email From: {0} ({1})</p><p>Message:</p><p>{2}</p>";
+                var message = new System.Net.Mail.MailMessage();
+                message.To.Add(new MailAddress("ganesh.radje@ynov.com"));  // replace with valid value 
+                message.From = new MailAddress("radjeganesh@hotmail.fr");  // replace with valid value
+                message.Subject = "Your email subject";
+                message.Body = string.Format(body, model.FromName, model.FromEmail, model.Message);
+                message.IsBodyHtml = true;
+
+                using (var smtp = new SmtpClient())
+                {
+                    var credential = new NetworkCredential
+                    {
+                        UserName = "ganesh.radje@outlook.fr",  // replace with valid value
+                        Password = "466OKR"  // replace with valid value
+                    };
+                    smtp.Credentials = credential;
+                    smtp.Host = "smtp-mail.outlook.com";
+                    smtp.Port = 587;
+                    smtp.EnableSsl = true;
+                    await smtp.SendMailAsync(message);
+                    return RedirectToAction("Sent");
+                }
+            }
+            return View(model);
+        }
+        public ActionResult Sent()
+        {
+            return View();
+        }
 
         public ActionResult Valres()
         {
---
+++ M2L_Asso/Models/Email.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace M2L_Asso.Models
+{
+    public class Email
+    {
+        [Required, Display(Name = "Your name")]
+        public string FromName { get; set; }
+        [Required, Display(Name = "Your email"), EmailAddress]
+        public string FromEmail { get; set; }
+        [Required]
+        public string Message { get; set; }
+
+    }
+}
